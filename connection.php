d6bb721b54b876f8a2a87da62a51a201e1f1d94d
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-06-22 17:20:56 +0900

    add

--- connection.php
+++ connection.php
@@ -1,5 +1,6 @@
 <?php
 
+if (isset($_POST['Submit'])){
 $con = mysqli_connect("localhost","root","","hotel_management") or die("Connection faild");
 
 // $name = $_POST['name'];
@@ -9,12 +10,25 @@ $con = mysqli_connect("localhost","root","","hotel_management") or die("Connecti
 // $course = $_POST['course'];
 // $sql = "INSERT INTO studentlogin(sname,sage,sgender,semail,scourse) VALUES('{$name}','{$age}','{$gender}','{$email}','{$course}')";
 
+$usernameuser = mysqli_real_escape_string($con,$_POST['Username']);
+$passworduser = $_POST['Password'];
 
-$sql = "Select from login ";
+$sql = "SELECT user_id FROM login WHERE username = '{$usernameuser}' AND password = '{$passworduser}' ";
 $result = mysqli_query($con,$sql) or die('Query Unsuccessfull');
 
-header("Location: http://localhost/website/my.php");
+if(mysqli_num_rows($result)>0){
+  while($row = mysqli_fetch_assoc($result)){
+    session_start();
+    $_SESSION["user_id"] = $row['user_id'];
+
+    header("Location: http://localhost/website/sidebar.php");
+  }
+}
+else{
+    echo'<div class="alert alert-danger"> Username and password are not matches.</div>';
+}
+
 
 mysqli_close($con);
-
+}
 ?>
--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
+    <?php include 'connection.php'?>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Login</title>
 </head>
@@ -14,17 +15,17 @@
             <div class="top-header">
                 <header>Login</header>
             </div>
-
+             <form action="" method ="POST">
             <div class="input-field">
-                <input type="text" class="input" placeholder="Username" required>
+                <input type="text" class="input" placeholder="Username" name='Username' required>
                 <i class="bx bx-user"></i>
             </div>
             <div class="input-field">
-                <input type="password" class="input" placeholder="Password" required>
+                <input type="password" class="input" placeholder="Password"  name='Password' required>
                 <i class="bx bx-lock-alt"></i>
             </div>
             <div class="input-field">
-                <input type="submit" class="submit" value="Login">
+                <input type="submit" class="submit" value="Login" name="Submit">
             </div>
 
             <div class="bottom">
@@ -33,6 +34,7 @@
                     <label for="check"> Remember Me</label>
                 </div>
             </div>
+             </form>
         </div>
     </div>
 </body>
