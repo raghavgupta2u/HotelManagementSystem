4ff289343f88938472a061501cd3e93d675039b6
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-07-24 16:30:20 +0530

    add

--- user/homepage.php
+++ user/homepage.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css2?family=Playwrite+US+Trad:wght@100..400&display=swap' rel='stylesheet'>
-    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="../homepage.css">
     <title>Hotel Magnificent</title>
 </head>
 <body>
--- user/login.php
+++ user/login.php
@@ -16,7 +16,7 @@ if (isset($_POST['Submit'])) {
       session_start();
       $_SESSION["user_id"] = $row['user_id'];
 
-      header("Location: http://localhost/website/admin/menu.php");
+      header("Location: http://localhost/website/admin/menu/Dashboard.php");
     }
   } else {
     echo '<div class="alert alert-danger"> Username and password are not matches.</div>';
@@ -29,7 +29,7 @@ if (isset($_POST['Submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="../login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>Login</title>
