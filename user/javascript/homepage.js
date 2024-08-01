a71eb7cbc7c3dcf278d1c942ef90483b850a17a7
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-08-02 01:02:42 +0530

    add

---
+++ user/javascript/homepage.js
@@ -0,0 +1,17 @@
+
+
+// const scroolRevealOption = {
+//     distance :"50px",
+//     origin: "left",
+//     duration: 1500,
+// };
+
+// ScrollReveal().reveal(".header__container p",{
+//     ...scroolRevealOption,
+//     origin: "right",
+// });
+
+// ScrollReveal().reveal(".header__container h5",{
+//     ...scroolRevealOption,
+//     origin: "right",
+// });
